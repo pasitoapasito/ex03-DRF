3195e96d8adbd0eccf57d899ca1c5146ffc3cfcc
DGK <pasito.dev@gmail.com>
2022-09-08 07:50:18 +0900

    feat: 유저 로그아웃 기능 구현 (#7)

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from users.views import KakaoSignInView
+from users.views.social_signin import KakaoSignInView
+from users.views.signout       import UserSignOutView
 
 
 urlpatterns = [
     path('/kakao-signin', KakaoSignInView.as_view()),
+    path('/signout', UserSignOutView.as_view()),
 ]
